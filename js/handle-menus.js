65143d91467b233769a2b2d4c390ac322af83e34
sebastianjvf <10047596+sebastianjvf@users.noreply.github.com>
2017-11-25 16:38:04 +0100

    feat: On-start-experience updated.

--- js/handle-menus.js
+++ js/handle-menus.js
@@ -8,6 +8,13 @@
 		let singleSongMenu = document.getElementById('single-song-menu');
 		let hasSingleSongMenu = false;
 		
+		let circleEnvironment = document.getElementById('little-circles');
+		let hasCircleEnvironment = false;
+		
+		let userCircle = document.getElementById('user-circle');
+		let perimetre = document.getElementById('perimetre');
+		let isPulsing = false;
+		
 		function showSongList() {
 			coloursAroundYou.style.bottom = '0';
 		}
@@ -24,8 +31,43 @@
 			singleSongMenu.style.bottom = '-100vh';
 		}
 		
-		showSongList();
+		function showCircleEnvironment() {
+			circleEnvironment.style.opacity = '1';
+			circleEnvironment.style.pointerEvents = 'auto';
+		}
+		
+		function hideCircleEnvironment() {
+			circleEnvironment.style.opacity = '0';
+			circleEnvironment.style.pointerEvents = 'none';
+		}
+		
+		function showPulse(text) {
+			// Update text
+			userCircle.innerHTML = text;
+			let pulse = document.createElement('div');
+			pulse.id = 'perimetre';
+			pulse.classList.add('pulsing');
+			console.log(pulse);
+			userCircle.appendChild(pulse);
+			
+			// Remove pointer and click events
+			userCircle.pointerEvents = 'none';
+		}
+		
+		function hidePulse() {
+			perimetre.classList.remove('pulsing');
+		}
+		
+		// Call on load up
+		hideSongList();
 		hideSingleSongMenu();
+		hideCircleEnvironment();
+		
+		userCircle.addEventListener('click', function () {
+			showPulse('Your colour.');
+			showCircleEnvironment();
+			showSongList();
+		});
 		
 		// Listen for selected events
 		window.addEventListener('circle-selected', function(event) {
