b9ad53e9230d6bafef15b9b7339b38b9b47a287e
sebastianjvf <10047596+sebastianjvf@users.noreply.github.com>
2017-11-26 07:12:01 +0100

    bug: Adding logs.

--- js/authenticate.js
+++ js/authenticate.js
@@ -2,6 +2,8 @@
 
 	window.addEventListener('load', function() {
 		
+		console.log('loaded');
+		
 		// Find the start button and menu bar
 		let startButton = document.getElementById('start-button');
 		let menuBar = document.getElementsByTagName('header')[0];
@@ -18,8 +20,11 @@
 		
 		startButton.addEventListener('click', function() {
 			
+			console.log('click');
+			
 			// Make post request
 			sendRequest(baseUrl + userEndpoint, 'POST', 'authenticated', function(event) { 
+				console.log('autenticated');
 				console.log(event.detail);
 			});
 			
